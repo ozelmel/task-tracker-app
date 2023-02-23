8e5cff225733623f10f77e3bc592ba588b1b6078
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-23 12:05:52 +0300

    state added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import "./App.css"
 
 function App() {
   
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: "SHOW ADD TASK BAR",
+    bgColor: "purple",
+  });
+  const handleShow = () => {
+    setShow(!show);
+    if (show) {
+      setBtnStyle({
+        name: "SHOW ADD TASK BAR",
+        bgColor: "purple",
+      });
+    } else {
+      setBtnStyle({
+        name: "CLOSE ADD TASK BAR",
+        bgColor: "red",
+      });
+    }
+  };
+  console.log(show);
   return (
     <header className="header">
       <h1>TASK TRACKER</h1>
-      <button className="btn">SHOW ADD TASK BAR</button>
-      <AddTaskForm />
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
+      </button>
+      {show && <AddTaskForm />}
     </header>
   );
 };
